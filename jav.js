c1de011b4bbf3fb588daead883d6840d870ce03a
Vitor Henrique Alves Seixas <vitor.seixas@fatec.sp.gov.br>
2026-06-19 07:05:46 -0300

    division,addition,subtraction and equal half-functional

--- jav.js
+++ jav.js
@@ -5,47 +5,61 @@ let keypad = document.querySelectorAll("button")
 
 let content = []
 let nonnumbers = []
+let operator_count = []
 
 for (let operator of operators.children) {
     nonnumbers.push(operator.textContent)
 };
 
-console.log(nonnumbers)
-
 function compute(operator) {
     let op_index = "";
+    let result = 0;
     
     switch (operator) {
         case "+":
-            op_index = content.indexOf("+")
-            num1 = +content.slice(0, op_index).join("")
-            console.log(num1)
-            num2 = +content.slice(op_index).join("")
-            console.log(num2)
+            op_index = content.indexOf("+");
+            num1 = +content.slice(0, op_index).join("");
+            num2 = +content.slice(op_index+1).join("");
 
-            console.log(num1 + num2)
+            result = num1 + num2;
+            display(result);
+            content.length = 0;
+            return result;
 
         case "-":
-            op_index = content.indexOf("-")
-            num1 = +content.slice(0, op_index).join("")
-            console.log(num1)
-            num2 = +content.slice(op_index).join("")
-            console.log(num2)
+            op_index = content.indexOf("-");
+            num1 = +content.slice(0, op_index).join("");
+            num2 = +content.slice(op_index+1).join("");
 
-            console.log(num1 - num2)
+            result = num1 - num2;
+            display(result);
+            content.length = 0;
+            return result;
 
         case "×":
-            op_index = content.indexOf("×")
-            num1 = +content.slice(0, op_index).join("")
-            console.log(num1)
-            num2 = +content.slice(op_index).join("")
-            console.log(num2)
+            op_index = content.indexOf("×");
+            num1 = +content.slice(0, op_index).join("");
+            num2 = +content.slice(op_index+1).join("");
 
-            console.log(num1 * num2)
+            result = num1 * num2;
+            display(result);
+            content.length = 0;
+            return result;
+        case "÷":
+            op_index = content.indexOf("÷");
+            num1 = +content.slice(0, op_index).join("");
+            num2 = +content.slice(op_index+1).join("");
 
+            result = num1 / num2;
+            display(result);
+            content.length = 0;
+            return result;
     };
 };
 
+function display(content) {
+    visualizer.textContent = content
+};
 
 keypad.forEach((key) => {
     let class_name = "class-" + key.textContent;
@@ -60,12 +74,13 @@ numberspad.addEventListener('click', (e) => {
 
 operators.addEventListener('click', (e) => {
     if (e.target.textContent == "=") {
-        compute(operator)
+        console.log(operator_count);
+        compute(operator_count.at(-1));
     } else {
     content.push(e.target.textContent);
+    operator_count.push(e.target.textContent)
     visualizer.textContent = content.join("");
     console.log(content)
-    let operator = e.target.textContent
     }
     
 })
